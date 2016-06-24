e005b7fd6161b5c1dab501e2e9030d6b9c1052fe
namco1992 <namco1992@gmail.com>
2016-06-24 18:31:56 +0800

    added, keyword cloud

--- app/static/js/stats.js
+++ app/static/js/stats.js
@@ -77,3 +77,45 @@ function plot_pie_chart(json_path, bindto){
 function transform(chart_type) {
     chart.transform(chart_type);
 }
+
+function plot_keywords_cloud(json_path, $sel){
+    function _plot(words){
+        var fill = d3.scale.category20();
+        var width = $($sel).width();
+        function draw(words){
+            console.log();
+            d3.select($sel).append("svg")
+                .attr("viewBox", "0 0 2000 1000")
+                .attr("width", width)
+                .attr("height", width*0.65)
+                .append("g")
+                .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function(d) { return d.size + "px"; })
+                .style("font-family", "Impact")
+                .style("fill", function(d, i) { return fill(i); })
+                .attr("text-anchor", "middle")
+                .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.text; });
+        };
+
+        var layout = d3.layout.cloud()
+            .size([2000, 1000])
+            .spiral("rectangular")
+            .random(function (x) {return 0.5;})
+            .words(words)
+            .padding(10)
+            .rotate(0)
+            .font("Impact")
+            .fontSize(function(d) { return 1.5*Math.sqrt(Math.pow(d.size, 1.4));})
+            .on("end", draw);
+
+        layout.start();
+    }
+
+    d3.json(json_path, raiser(_plot));
+}
