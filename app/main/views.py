380b7538e8b0155ec3a5eceec9cc290d84bf9bed
namco <namco1992@gmail.com>
2016-06-26 23:39:20 +0800

    modified

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,11 @@ from . import main
 from flask import render_template, jsonify, flash, request, current_app, url_for, Response, g, abort
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('about')
+def about_page():
+    return render_template('about.html')
